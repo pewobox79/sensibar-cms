e3d01607d00eb4d7cf70a28d58bc05bf5bf575ee
aviareps_pwolf <pwolf@aviareps.com>
2024-10-18 16:58:26 +0200

    updated cors settings

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: false,
+      origin: ['http://localhost:3000', 'https://sensibar-coaching.de', 'https://www.sensibar-coaching.de'],  // Specify the allowed origins
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],  // Allowed methods
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],  // Allowed headers
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
